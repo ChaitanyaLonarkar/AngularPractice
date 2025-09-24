30f7277c53e356f7f37de0e22eb7b35f8c85446e
Chaitanyalonarkar <chaitanyalonarkar@gmail.com>
2025-09-24 23:13:26 +0530

    used routerlink to prevent reload when going to new route

--- first-app/src/app/app.routes.ts
+++ first-app/src/app/app.routes.ts
@@ -1,12 +1,17 @@
 import { Routes } from '@angular/router';
 import { Contact } from './pages/contact/contact';
 import { About } from './pages/about/about';
+import { Home } from './pages/home/home';
+
 
 export const routes: Routes = [
   {
     path: '',
-    redirectTo: 'home',
+    // redirectTo: 'home',
     pathMatch: 'full',
+    component:Home,
+    // loadComponent: ()=> {return import('./pages/home/home').then((m)=>m.Home)}
+
   },
   {
     path: 'contact',
