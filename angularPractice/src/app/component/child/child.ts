448ad2730e243d18158e085da7754de99e8a621e
Chaitanyalonarkar <chaitanyalonarkar@gmail.com>
2025-10-08 23:19:17 +0530

    custom pipes done

--- angularPractice/src/app/component/child/child.ts
+++ angularPractice/src/app/component/child/child.ts
@@ -1,9 +1,10 @@
-import { JsonPipe } from '@angular/common';
+import { JsonPipe, UpperCasePipe } from '@angular/common';
 import { Component, EventEmitter, Input, Output } from '@angular/core';
+import { CustomPipePipe } from '../../pipe/custom-pipe-pipe';
 
 @Component({
   selector: 'app-child',
-  imports: [JsonPipe],
+  imports: [JsonPipe, UpperCasePipe,CustomPipePipe],
   templateUrl: './child.html',
   styleUrl: './child.css',
 })
